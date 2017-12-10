02ffc072e689af0500c50e7daab72de0e3d4ffe5
Rithik Yelisetty <rithik@gmail.com>
2017-12-10 14:54:01 -0500

    Add first set of files

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_sqlalchemy import SQLAlchemy
+import os
+from models import User
+import settings
+import sys
+from database import db_session
+import json
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+app.config['APP_SETTINGS'] = settings.APP_SETTINGS
+app.config['SQLALCHEMY_DATABASE_URI'] = settings.DB_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.TRACK_MODIFICATIONS
+app.secret_key = settings.SECRET_KEY
+
+@app.route('/')
+def home_page():
+    return "Home Page"
+
+if __name__ == '__main__':
+    app.run()
---
+++ models.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from sqlalchemy import Column, Integer, String, Float
+from database import Base
+import settings
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = settings.DB_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.TRACK_MODIFICATIONS
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(length=50))
+    last_name = Column(String(length=50))
+    email = Column(String(length=120), unique=True)
+    password = Column(String(length=50))
+
+    def __init__(self, first_name, last_name, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return '<email {}>'.format(self.email)
