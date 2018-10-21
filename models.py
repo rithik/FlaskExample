efadc23a09e08ef0ca08c44d921f5781068b41ee
Rithik Yelisetty <rithik@gmail.com>
2018-10-20 21:15:24 -0400

    Improve password security by hashing password

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from flask import Flask
 from sqlalchemy import Column, Integer, String, Float
 from database import Base
 import settings
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = settings.DB_URL
@@ -14,13 +15,19 @@ class User(Base):
     first_name = Column(String(length=50))
     last_name = Column(String(length=50))
     email = Column(String(length=120), unique=True)
-    password = Column(String(length=50))
+    password = Column(String(length=100))
 
     def __init__(self, first_name, last_name, email, password):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-        self.password = password
+        self.password = self.set_password(password)
 
     def __repr__(self):
         return '<email {}>'.format(self.email)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
